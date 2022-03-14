149fa1469819b07eb312a75615c873ad1e29ee5d
Roberto Montalti <roberto.montalti00@gmail.com>
2022-03-14 21:06:18 +0100

    Added queue principal methods

--- user-queue.ts
+++ user-queue.ts
@@ -1,18 +1,38 @@
 import Denque from "denque";
-import { YoutubeSong } from "./youtube";
+
+type MediaUrl = string;
 
 class SongQueue {
-    private deque: Denque<YoutubeSong>;
+    private deque: Denque<MediaUrl>;
 
-    public constructor(songs: string[]) {
-        this.deque = new Denque();
-    }
-
-    public next(): YoutubeSong | null {
+    public next(): MediaUrl | null {
         if (this.deque.size() == 0) {
             return null;
         }
 
+        let item = this.deque.shift();
+
+        return item !== undefined ? item : null;
+    }
+
+    public push(song: MediaUrl): void {
+        this.deque.push(song);
+    }
+
+    public skipN(n: number): MediaUrl | null {
+        if (n - 1 >= this.deque.size()) {
+            return null;
+        }
+
+        this.deque.splice(0, n - 1);
         return this.deque.shift();
     }
+
+    public clear(): void {
+        this.deque.clear();
+    }
+}
+
+export {
+    SongQueue
 }
